2a3b4d1bb49d1bf7c85d2ab6a0c2d219321da32d
Cesar Richard <cesar.richard@weezevent.com>
2020-06-20 15:53:25 +0200

    Added pump svg component

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -6,7 +6,7 @@ import HomeScreen from "../screens/HomeScreen";
 import AboutScreen from "../screens/AboutScreen";
 
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = "Control";
+const INITIAL_ROUTE_NAME = "Home";
 
 export default function BottomTabNavigator({ navigation, route }) {
   // Set the header title on the parent stack navigator depending on the
